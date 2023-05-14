3718500e0eab48b4423b7d5b50de0b6da1d8defb
Carolina Westphal <carolinawestphal@gmail.com>
2023-05-14 16:45:41 -0300

    feat: add csv file handling

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
+const fs = require('fs');
 const express = require('express')
+const multer = require('multer');
+const csv = require('fast-csv');
+
+const upload = multer({ dest: 'tmp/' });
+
 const app = express()
 const port = 3000
 
 const db = require("./models");
 db.sequelize.sync();
+
+app.post('/check-file', upload.single('file'), function (req, res) {
+    const fileRows = [];
+  
+    csv.fromPath(req.file.path)
+      .on("data", function (data) {
+        fileRows.push(data);
+      })
+      .on("end", function () {
+        console.log(fileRows)
+        fs.unlinkSync(req.file.path);
+      })
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
