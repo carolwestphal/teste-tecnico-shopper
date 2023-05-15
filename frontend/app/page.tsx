d1714e8458ff7c2390c0226dd864eadc38aa694f
Carolina Westphal <carolinawestphal@gmail.com>
2023-05-15 00:11:23 -0300

    feat: improve file upload result notifications

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,11 @@ import { useState, useRef } from "react";
 export default function Home() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [errors, changeErrors] = useState([]);
+  const [successMessage, changeSuccessMessage] = useState('');
+
+  function onFileInputChange() {
+    changeSuccessMessage('');
+  }
 
   async function onVerifyFile() {
     if (!fileInputRef.current?.files?.length) {
@@ -20,6 +25,10 @@ export default function Home() {
       }
     });
     changeErrors(response.data);
+
+    if (response.data.length === 0) {
+      changeSuccessMessage('Arquivo pronto para envio!');
+    }
   }
 
   async function onSendFile() {
@@ -41,6 +50,7 @@ export default function Home() {
     if (response.status !== 200) {
       changeErrors(response.data);
     }
+    changeSuccessMessage('Seu arquivo foi enviado com sucesso!');
   }
 
   return (
@@ -49,7 +59,7 @@ export default function Home() {
         <h3>Enviar arquivo para atualizar produtos</h3>
 
         <div className="form-group">
-          <input type="file" ref={fileInputRef} />
+          <input type="file" onChange={onFileInputChange} ref={fileInputRef} />
         </div>
 
         <div className="form-group button-bar">
@@ -71,7 +81,17 @@ export default function Home() {
 
       {errors.length > 0 && (
         <div>
-          {JSON.stringify(errors)}
+          {errors.map((error: { lineNumber: number, errors: string }) => (
+            <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400">
+              Erro na linha {error.lineNumber}: {error.errors}
+            </div>
+          ))}
+        </div>
+      )}
+      
+      {successMessage && (
+        <div className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+          {successMessage}
         </div>
       )}
     </main>
