84e0faae667bcbea109df56c89e7da35b2fa6653
Carolina Westphal <carolinawestphal@gmail.com>
2023-05-16 01:00:26 -0300

    fix: change button names

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -67,14 +67,14 @@ export default function Home() {
             onClick={onVerifyFile}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Verificar
+            Validar
           </button>
 
           <button
             onClick={onSendFile}
             className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
           >
-            Enviar
+            Atualizar
           </button>
         </div>
       </div>
